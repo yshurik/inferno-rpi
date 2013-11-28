389df6b181bb1160103b81d8164a3c81c2e854f7
yshurik <yshurik@gmail.com>
2013-11-28 12:20:23 +0100

    Lab 3, R-Pi Booting process: prepare our boot setup based on U-Boot to load inferno kernel using tftp (http://lynxline.com/lab-3-r-pi-booting-process/)

---
+++ os/rpi/misc/boot-tftp.cmd
@@ -0,0 +1,11 @@
+set autoload no
+set serverip 10.0.55.112
+set bootfile irpi
+set loadaddr 7FE0
+if usb start; then
+    if dhcp; then
+        if tftpboot ${loadaddr} irpi; then
+            go 8000;
+        fi;
+    fi;
+fi;
---
+++ os/rpi/misc/make_boot.scr
@@ -0,0 +1,1 @@
+mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n 'U-Boot script' -d boot-tftp.cmd boot.scr
