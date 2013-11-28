2e1c504d1dc8c68d0b42903d4f5a1ad531671fed
yshurik <yshurik@gmail.com>
2013-11-28 14:42:01 +0100

    Lab 7, linking, planning next: complete the building process by adding stubs to have it linked (http://lynxline.com/lab-7-linking-more-initialization/)

--- os/rpi/fns.h
+++ os/rpi/fns.h
@@ -1,8 +1,10 @@
 
 #define KADDR(p)    ((void *)p)
 #define PADDR(p)    ((ulong)p)
+#define	coherence()		/* nothing needed for uniprocessor */
+#define procsave(p)
 
-int		waserror();
+int		waserror(void);
 void    (*screenputs)(char*, int);
 
 #include "../port/portfns.h"
--- os/rpi/main.c
+++ os/rpi/main.c
@@ -35,3 +35,46 @@ main() {
 	pl011_puts(s);
 	for (;;);
 }
+
+#include "u.h"
+#include "../port/lib.h"
+#include "dat.h"
+#include "mem.h"
+
+Conf conf;
+Mach *m = (Mach*)MACHADDR;
+Proc *up = 0;
+
+#include "../port/uart.h"
+PhysUart* physuart[1];
+
+int		waserror(void) { return 0; }
+int		splhi(void) { return 0; }
+void	splx(int) { return; }
+int		spllo(void) { return 0; } 
+void	splxpc(int) { return; }
+int		islo(void) { return 0; }
+int		setlabel(Label*) { return 0; }
+void	gotolabel(Label*) { return; }
+ulong	getcallerpc(void*) { return 0; }
+int		segflush(void*, ulong) { return 0; }
+void	idlehands(void) { return; }
+void 	kprocchild(Proc *p, void (*func)(void*), void *arg) { return; }
+ulong	_tas(ulong*) { return 0; }
+ulong	_div(ulong*) { return 0; }
+ulong	_divu(ulong*) { return 0; }
+ulong	_mod(ulong*) { return 0; }
+ulong	_modu(ulong*) { return 0; }
+
+void	setpanic(void) { return; }
+void	dumpstack(void) { return; }
+void	exit(int) { return; }
+void	reboot(void) { return; }
+void	halt(void) { return; }
+
+Timer*	addclock0link(void (*)(void), int) { return 0; }
+void	clockcheck(void) { return; }
+
+void	fpinit(void) {}
+void	FPsave(void*) {}
+void	FPrestore(void*) {}
